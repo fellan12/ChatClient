af76b33d8fff36253a924c788ee302c734ca4c0c
fellan12 <fellan12@gmail.com>
2014-05-02 14:31:22 +0200

    Changed name of project

--- INDA13 Projekt/src/ClientWindow.java
+++ INDA13 Projekt/src/ClientWindow.java
@@ -9,30 +9,55 @@ import java.awt.event.*;
 public class ClientWindow extends JFrame {
 
 	private JPanel panel;
-	private JTextField textField;
-	private JButton sendButton;
 
 	/**
-	 * Create the frame.
+	 * Constructor for ClientWindow
 	 */
 	public ClientWindow() {
 		define();
 	}
-
+	
+	/**
+	 * Defines the content in ClientWindow
+	 * 
+	 * All its content is defines and creates here
+	 */
 	public void define(){
 		panel = new JPanel();
 		panel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		panel.setLayout(null);
 		setTitle("Chatroom");
+		
+		//Menubar
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		//Menu - File
+		JMenu fileMenu = new JMenu("File");
+		menuBar.add(fileMenu);
+		
+		//MenuItem - Open
+		JMenuItem mt_Open = new JMenuItem("Open");
+		fileMenu.add(mt_Open);
+		
+		//MenuItem - Exit
+		JMenuItem mt_Exit = new JMenuItem("Exit");
+		mt_Exit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				System.exit(0);
+			}
+		});
+		fileMenu.add(mt_Exit);
+	
 		setContentPane(panel);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 1000, 800);
 		setResizable(true);								//True if screen objects obey resizing.
 		
 		//Message - TextField
-		textField = new JTextField();
+		JTextField textField = new JTextField();
 		textField.setFont(new Font("Arial", Font.PLAIN, 15));
-		textField.setBounds(7, 728, 870, 30);
+		textField.setBounds(7, 691, 859, 30);
 		panel.add(textField);
 		textField.setColumns(10);
 		
@@ -43,7 +68,7 @@ public class ClientWindow extends JFrame {
 						System.out.println("Sent");
 					}
 				});
-				sendButton.setBounds(885, 728, 100, 29);
+				sendButton.setBounds(873, 691, 100, 30);
 				panel.add(sendButton);
 	}
 	
