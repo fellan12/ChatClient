2b179e184544ebc59ca6a337946dab46e8e715c3
fellan12 <fellan12@gmail.com>
2014-05-02 14:31:22 +0200

    Changed name of project

--- INDA13 Projekt/src/LoginWindow.java
+++ INDA13 Projekt/src/LoginWindow.java
@@ -1,5 +1,7 @@
 import java.awt.BorderLayout;
+import java.awt.Component;
 import java.awt.EventQueue;
+import java.awt.Graphics;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -12,9 +14,13 @@ import javax.swing.JLabel;
 import javax.swing.JButton;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
+
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+
 import javax.swing.JScrollBar;
+import java.awt.event.ContainerAdapter;
+import java.awt.event.ContainerEvent;
 
 
 public class LoginWindow extends JFrame {
@@ -24,18 +30,18 @@ public class LoginWindow extends JFrame {
 	private JTextField textName;
 	private JTextField textPort;
 	private boolean error = false;
-	
+
 	/**
-	 * Create the application.
+	 * Constructor for LoginWindow
 	 */
 	public LoginWindow() {
-		setResizable(false);	
-		setTitle("Login");		//Windom name
 		define();
 	}
 
 	/**
-	 * Defines the contents of the frame.
+	 * Defines the contents in the LoginWindow.
+	 * 
+	 * All its content is defines and creates here
 	 */
 	private void define(){
 		try {
@@ -43,7 +49,7 @@ public class LoginWindow extends JFrame {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 		//Frame
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(300, 300, 355, 530);				//TODO set center though resolution
@@ -51,47 +57,42 @@ public class LoginWindow extends JFrame {
 		panel.setBorder(new EmptyBorder(5,5,5,5));
 		setContentPane(panel);
 		panel.setLayout(null);
-		
+		setResizable(false);
+		setTitle("Login");
+
 		//IP-Adress  - TextField
 		textIp = new JTextField();
 		textIp.setBounds(84, 145, 180, 32);
 		panel.add(textIp);
 		textIp.setColumns(10);
-		
+
 		//Screen Name  - TextField
 		textName = new JTextField();
 		textName.setColumns(10);
 		textName.setBounds(84, 70, 180, 32);
 		panel.add(textName);
-		
+
 		//Port - TextField
 		textPort = new JTextField();
 		textPort.setColumns(10);
 		textPort.setBounds(84, 220, 180, 32);
 		panel.add(textPort);
-		
+
 		//Name - Label
 		JLabel lblName = new JLabel("Name:");
 		lblName.setBounds(155, 50, 38, 16);
 		panel.add(lblName);
-		
+
 		//IP-Adress - Label
 		JLabel lblIpadress = new JLabel("IP-Adress:");
 		lblIpadress.setBounds(144, 125, 61, 16);
 		panel.add(lblIpadress);
-		
+
 		//Port - Label
 		JLabel lblPort = new JLabel("Port:");
 		lblPort.setBounds(155, 200, 38, 16);
 		panel.add(lblPort);
-		
-		if(error == true){
-			//Error - Label
-			JLabel lblError = new JLabel("Check that alla the inpus are correct");
-			lblError.setBounds(124, 400, 38, 16);
-			panel.add(lblError);
-		}
-		
+
 		//Login - Button
 		JButton btnNewButton = new JButton("Login");
 		btnNewButton.addActionListener(new ActionListener() {		//ActionListener for Clicking the button
@@ -106,19 +107,18 @@ public class LoginWindow extends JFrame {
 					login(name, ip, port);
 					System.out.println("Logged in");
 				}else{
-					error = true;
 					System.out.println("Error");
 					System.out.println("Name: " + textName.getText());
 					System.out.println("IP: " + textIp.getText());
 					System.out.println("Port: " + textPort.getText());
+					//TODO felhantering: fixa en röd label som säger att inputen är fel
 				}
 			}
 		});
 		btnNewButton.setBounds(124, 347, 100, 30);
 		panel.add(btnNewButton);
-		
 	}
-	
+
 	/**
 	 * Login sequence
 	 */
