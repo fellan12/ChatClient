7c986bb724535f340008f63577bdfd289488c4f2
fellan12 <fellan12@gmail.com>
2014-05-03 17:14:39 +0200

    Lagt till MessageInput och Send button

--- INDA13 Projekt/src/ClientWindow.java
+++ INDA13 Projekt/src/ClientWindow.java
@@ -13,6 +13,7 @@ public class ClientWindow extends JFrame {
 	private String ip;
 	private int port;
 	private JMenuBar menuBar;
+	private JTextField txtMessage;
 
 	/**
 	 * Constructor for ClientWindow
@@ -31,33 +32,54 @@ public class ClientWindow extends JFrame {
 	 * All its content is defines and creates here
 	 */
 	public void define(){
+		//Panel
 		panel = new JPanel();
 		panel.setAlignmentY(Component.TOP_ALIGNMENT);
 		panel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(panel);
 		
+		//Frame Settings
 		setTitle("Chatroom");
 		setVisible(true);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(1000, 800);
-		setResizable(true);					//True if screen objects obey resizing.
+		setResizable(true);	
 		
-		//Layout
-		GridBagLayout gbl_panel = new GridBagLayout();
-		gbl_panel.columnWidths = new int[]{40, 940, 20};		//sum = 1000
-		gbl_panel.rowHeights = new int[]{75, 640, 75};			//sum = 800
-		gbl_panel.columnWeights = new double[]{1.0, 1.0, 0.0};
-		gbl_panel.rowWeights = new double[]{1.0, Double.MIN_VALUE};
-		panel.setLayout(gbl_panel);
+		//Layout -GridBagLayout
+		GridBagLayout layout = new GridBagLayout();
+		layout.columnWidths = new int[]{40, 910, 40, 10};						//sum = 1000
+		layout.rowHeights = new int[]{75, 660, 55};								//sum = 800
+		layout.columnWeights = new double[]{1.0, 1.0, 0.0};
+		layout.rowWeights = new double[]{1.0, Double.MIN_VALUE};
+		panel.setLayout(layout);
 		
+		//On Screen History - TextArea
 		JTextArea textHistory = new JTextArea();
-		GridBagConstraints gbc_textHistory = new GridBagConstraints();
-		gbc_textHistory.insets = new Insets(0, 0, 5, 5);
-		gbc_textHistory.fill = GridBagConstraints.BOTH;
-		gbc_textHistory.gridx = 1;
-		gbc_textHistory.gridy = 1;
-		panel.add(textHistory, gbc_textHistory);
+		GridBagConstraints textHistoryConstrains = new GridBagConstraints();
+		textHistoryConstrains.gridwidth = 2;
+		textHistoryConstrains.insets = new Insets(0, 0, 5, 5);
+		textHistoryConstrains.fill = GridBagConstraints.BOTH;
+		textHistoryConstrains.gridx = 1;
+		textHistoryConstrains.gridy = 1;
+		panel.add(textHistory, textHistoryConstrains);
 		
+		//Message Field - TextField
+		txtMessage = new JTextField();
+		GridBagConstraints txtMessageConstrains = new GridBagConstraints();
+		txtMessageConstrains.fill = GridBagConstraints.HORIZONTAL;
+		txtMessageConstrains.insets = new Insets(0, 0, 0, 5);
+		txtMessageConstrains.gridx = 1;
+		txtMessageConstrains.gridy = 2;
+		panel.add(txtMessage, txtMessageConstrains);
+		txtMessage.setColumns(10);
+		
+		//Send - Button
+		JButton sendButton = new JButton("Send");
+		GridBagConstraints sendButtonConstrains = new GridBagConstraints();
+		sendButtonConstrains.insets = new Insets(0, 0, 0, 5);
+		sendButtonConstrains.gridx = 2;
+		sendButtonConstrains.gridy = 2;
+		panel.add(sendButton, sendButtonConstrains);
 		
 		//Menubar
 		menuBar = new JMenuBar();
@@ -69,17 +91,17 @@ public class ClientWindow extends JFrame {
 		menuBar.add(fileMenu);
 
 		//MenuItem - Open
-		JMenuItem mt_Open = new JMenuItem("Open");
-		fileMenu.add(mt_Open);
+		JMenuItem menuItemOpen = new JMenuItem("Open");
+		fileMenu.add(menuItemOpen);
 
 		//MenuItem - Exit
-		JMenuItem mt_Exit = new JMenuItem("Exit");
-		mt_Exit.addActionListener(new ActionListener() {
+		JMenuItem menuItemExit = new JMenuItem("Exit");
+		menuItemExit.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				System.exit(0);
 			}
 		});
-		fileMenu.add(mt_Exit);
+		fileMenu.add(menuItemExit);
 	}
 }
 
