c8ca202a03fb8b8ac7c0112a58a2856e53c99e49
fellan12 <fellan12@gmail.com>
2014-05-15 15:00:28 +0200

    stuff

--- INDA13 Projekt/src/Server/ServerWindow.java
+++ INDA13 Projekt/src/Server/ServerWindow.java
@@ -26,10 +26,10 @@ public class ServerWindow extends JFrame {
 	JPanel panel; // The contents of this window.
 	JTextField port; // The text field in which the user enters the port she wishes to start the server on.
 	JLabel running; // Shows whether the server is running in the panel.
-	
+
 	private static Server server; // The server for which the GUI is made.
 	private static CountDownLatch setUp;
-		
+
 	/**
 	 * Creates and shows a new GUI for a server.
 	 */
@@ -38,7 +38,7 @@ public class ServerWindow extends JFrame {
 		define();
 		setVisible(true);
 	}
-	
+
 	/**
 	 * Defines the content of the server window.
 	 */
@@ -49,36 +49,48 @@ public class ServerWindow extends JFrame {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 		// Frame
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(750, 300, getWidth(), getHeight());
 		setResizable(false);
 		setTitle("ChatServer"); // TODO: Change?
 		setSize(366, 531);
-		
+
 		// Panel
 		panel = new JPanel();
 		panel.setBorder(new EmptyBorder(5,5,5,5));
 		panel.setLayout(null);
 		setContentPane(panel);
-		
+
 		// Port - TextField
 		port = new JTextField("");
+		port.addActionListener(
+				new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						// Only one server can run at a time.
+						if (server != null) { // Server already running.
+							JOptionPane.showMessageDialog(null, "You have already started a server. "
+									+ "In order to start a new server you must first stop the existing one.");	
+						} else { 
+							startServer(port.getText());	
+						}
+					}
+				});
 		port.setBounds(90, 145, 180, 32); 
 		panel.add(port);
 		port.setColumns(10);
-		
+
 		// Port - Label
 		JLabel lblIpadress = new JLabel("Port:"); 
 		lblIpadress.setBounds(165, 125, 61, 16);  
 		panel.add(lblIpadress);
-		
+
 		// Running - Label
 		running = new JLabel("Server not running.");
 		running.setBounds(133, 175, 200, 50);
 		panel.add(running);
-		
+
 		// Start server - Button
 		JButton start = new JButton("Start server"); 
 		start.setBounds(130, 250, 100, 30); 
@@ -96,7 +108,7 @@ public class ServerWindow extends JFrame {
 					}
 				});
 		panel.add(start);
-		
+
 		// Shut down server - Button
 		JButton stop = new JButton("Shut down"); 
 		stop.setBounds(130, 300, 100, 30); 
@@ -117,7 +129,7 @@ public class ServerWindow extends JFrame {
 				});
 		panel.add(stop);
 	}
-	
+
 	/**
 	 * Try to set up a server on the given port. Check the port number 
 	 * to see if it's valid and available. If it is, set up a new server 
@@ -129,7 +141,7 @@ public class ServerWindow extends JFrame {
 		int portNumber;
 		try {
 			portNumber = Integer.parseInt(port);
-			
+
 			if (portNumber > 0 && portNumber <= 65535) { // Valid port numbers.
 				server = new Server(portNumber); 
 				if (server != null) {
@@ -148,7 +160,7 @@ public class ServerWindow extends JFrame {
 			JOptionPane.showMessageDialog(null, "Port unavailable.");			
 		}
 	}
-	
+
 	/**
 	 * Creates a new server window. 
 	 * 
@@ -162,7 +174,7 @@ public class ServerWindow extends JFrame {
 	 */
 	public static void main(String[] args) {
 		new ServerWindow();
-		
+
 		while (true) {
 			// Wait until the server has been set up.
 			try {
@@ -171,11 +183,11 @@ public class ServerWindow extends JFrame {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			
+
 			while (server != null) { // Server was successfully set up.
 				server.acceptRequest(); 
 			}
-			
+
 			System.out.println("not running");
 		}
 	}
